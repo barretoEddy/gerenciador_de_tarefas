ce1146226980062a6a69bad7b81b2f451e4f2ef0
barretoEddy <barreto.eduardo@ufba.br>
2025-04-19 15:07:18 -0300

    projeto finalizado de um gerenciador de tarefas simples

--- projeto final/script.js
+++ projeto final/script.js
@@ -5,6 +5,8 @@ class Tarefas {
     divFeito = document.getElementById("div_JaFeita")
 
    tarefasfazer = [] // Array para armazenar as tarefas a fazer
+   tarefasFazendo = []
+   tarefasFeita = []
 
 
     CriarTarefa() {
@@ -15,19 +17,52 @@ class Tarefas {
         this.Mostrar()
         
     }
-    MudarTarefa() {
+    MudarTarefa(index) {
+        if(index != null) {
+            this.tarefasFazendo.push({nome: this.tarefasfazer[index].nome, i: "realizando"})
+            this.tarefasfazer.splice(index, 1)
+        }
+        
+        this.divFazendo.innerHTML = this.tarefasFazendo.map((tarefa, i) =>
+            `
+            <div id="Fazendo" class = "exibirtarefa">
+                <h2>${tarefa.nome}</h2>
+                <button class="buttonTarefa" onclick = "obj.ConcluirTarefa(${i})">=></button>
+            </div>
+            `).join("")
+            this.Mostrar()
+
 
     }
-    ExcluirTarefa() {
+    ConcluirTarefa(index) {
+        if (index != null) {
+            this.tarefasFeita.push({nome: this.tarefasFazendo[index].nome, i: "terminado"})
+            this.tarefasFazendo.splice(index, 1)
+        }
+        
+        this.divFeito.innerHTML = this.tarefasFeita.map((tarefa, i) =>
+            `
+            <div id="Feito" class = "exibirtarefa">
+                <h2>${tarefa.nome}</h2>
+                <button class="buttonTarefa" onclick = "obj.ExcluirTarefa(${i})">X</button>
+            </div>
+            `).join("")
+            this.MudarTarefa(null)
     
+    }
+
+    ExcluirTarefa(index) {
+        this.tarefasFeita.splice(index, 1)
+        this.ConcluirTarefa(null) 
+
     }
 
     Mostrar() {
         this.divFazer.innerHTML = this.tarefasfazer.map((tarefa, i) =>
             `
-            <div class = "exibirtarefa">
+            <div id ="Fazer" class = "exibirtarefa">
                 <h2>${tarefa.nome}</h2>
-                <button class="buttonTarefa">=></button>
+                <button class="buttonTarefa" onclick = "obj.MudarTarefa(${i})">=></button>
             </div>
             `).join("")
         }
